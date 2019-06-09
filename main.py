20ad171dbf49a6e2154922fa868e305aac1094b8
zahramajd <zahra.majdabadi95@gmail.com>
2019-06-10 01:37:24 +0430

    compute distances

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import math
+from node import Node
 # Clustering using a random walk based distance measure
 
 #TODO:
@@ -7,8 +8,23 @@ import pandas as pd
 # spanning tree
 # ETC
 
+k = 3
+
+def euclidean_distance(x1,x2):
+    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x1, x2)]))
 
 data = pd.read_csv("data.csv")
 data = data.as_matrix()
 
-print(data[0][0])
+
+graph = []
+distances = []
+
+for i in range(len(data)):
+    graph.append(Node(data[i][0], data[i][1], i))
+    for j in range(i+1, len(data)):
+        distances.append({'i':i , 'j':j, 'dis':euclidean_distance(data[i], data[j])})
+
+
+print(len(data))
+print(len(distances))
---
+++ node.py
@@ -0,0 +1,12 @@
+
+
+class Node:
+
+    def __init__(self, x, y, id):
+
+        self.x = x
+        self.y = y
+
+        self.id = id
+        self.neighbors = []
+
