081aa2228aec98b8a506e2269b7ce99804d164b1
zahramajd <zahra.majdabadi95@gmail.com>
2019-06-09 23:40:43 +0430

    read data

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import pandas as pd
 
 # Clustering using a random walk based distance measure
+
+#TODO:
+# euclidian distance
+# spanning tree
+# ETC
+
+
+data = pd.read_csv("data.csv")
+data = data.as_matrix()
+
+print(data[0][0])
